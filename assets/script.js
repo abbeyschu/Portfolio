e9669f2105becff83ecbe604a6e6c7f7806adb2f
Abbey Schueller <schuellerabbey@gmail.com>
2021-05-24 21:05:24 -0700

    added modals

---
+++ assets/script.js
@@ -0,0 +1,46 @@
+// opening and closing modals
+
+    var open1 = document.getElementById('open1');
+    var modal1 = document.getElementById('modal1');
+    var closeModal1 = document.getElementById('close1');
+    var background1 = document.getElementById('background1');
+    
+    open1.addEventListener("click", function(){
+        modal1.style.display = 'block'   
+    });
+    closeModal1.addEventListener("click", function(){
+        modal1.style.display = 'none'
+    });
+    background1.addEventListener("click", function(){
+        modal1.style.display = 'none'
+    });
+
+    var open2 = document.getElementById('open2');
+    var modal2 = document.getElementById('modal2');
+    var closeModal2 = document.getElementById('close2');
+    var background2 = document.getElementById('background2');
+
+    open2.addEventListener("click", function(){
+        modal2.style.display = 'block'   
+    });
+    closeModal2.addEventListener("click", function(){
+        modal2.style.display = 'none'
+    });
+    background2.addEventListener("click", function(){
+        modal2.style.display = 'none'
+    });
+
+    var open3 = document.getElementById('open3');
+    var modal3 = document.getElementById('modal3');
+    var closeModal3 = document.getElementById('close3');
+    var background3 = document.getElementById('background3');
+
+    open3.addEventListener("click", function(){
+        modal3.style.display = 'block'   
+    });
+    closeModal3.addEventListener("click", function(){
+        modal3.style.display = 'none'
+    });
+    background3.addEventListener("click", function(){
+        modal3.style.display = 'none'
+    });
